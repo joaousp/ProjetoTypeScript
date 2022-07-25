c79736063fdd7d526f98c2ffcb482f34875264bd
João Paulo Garcia Martinelli de Oliveira <joao.martinelli@outlook.com.br>
2022-07-24 22:38:53 -0300

    falta exercicio 4

---
+++ desafio1.ts
@@ -0,0 +1,13 @@
+
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+class employee {
+    code:number;
+    name:string;
+    constructor(code,name){
+        this.code=code,
+        this.name=name
+    }
+}
+
+let funcionario = new employee(10,'jhon');
---
+++ desafio2.ts
@@ -0,0 +1,27 @@
+// Como podemos melhorar o esse código usando TS? 
+
+class pessoa{
+    nome:string;
+    idade:number;
+    profissao:string;
+    constructor(nome,idade,profissao){
+        this.nome=nome;
+        this.idade=idade;
+        this.profissao=profissao;
+    }
+
+}
+
+
+let pessoa1 = new pessoa("maria",29,"atriz");
+
+
+let pessoa2 = new pessoa("roberto",19,"Padeiro");
+
+
+let pessoa3 = new pessoa("laura",32,"Atriz");
+
+ 
+
+let pessoa4 = new pessoa("carlos",19,"padeiro");
+
